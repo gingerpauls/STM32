0f6650346c90eb3fac13bdec22a2794fdcce104b
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-16 23:47:36 -0800

    more abstraction - timer still not working

--- BARE/Src/main.c
+++ BARE/Src/main.c
@@ -29,53 +29,21 @@
 void HSI_PLL_CLK_EN(void);
 void HSE_PLL_CLK_EN(void);
 
+void TIM2_CONFIG(void);
+
+void SYSTICK_CONFIG(void);
+void GPIO_CONFIG(void);
+
 void delay(uint32_t time);
 
 int main(void)
 {
 	SystemCoreClockUpdate();
 
-
-	PWR->CR |= PWR_CR_VOS;
-
-
-	FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS;
-
-
-	SysTick->LOAD &= SysTick_LOAD_RELOAD_Msk; // this is about 1 second
-	//NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
-	SysTick->VAL &= ~SysTick_VAL_CURRENT_Msk;
-	SysTick->CTRL |= 	SysTick_CTRL_CLKSOURCE_Msk 	|
-						SysTick_CTRL_TICKINT_Msk 	|
-						SysTick_CTRL_ENABLE_Msk;
-
-
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIODEN;
-
-	// TIM2 clocked by pre-scaler output "CK_CNT"
-	// enable CEN before CK_CNT
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_PWREN;
-	TIM2->ARR &= ~TIM_ARR_ARR;
-	TIM2->ARR = 0x1F4;
-	//NVIC->ISER
-	NVIC_SetPriority (TIM2_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
-	TIM2->CNT &= ~TIM_CNT_CNT;
-	TIM2->EGR |= TIM_EGR_UG;
-	TIM2->SMCR |= TIM_SMCR_ECE; // external clock enable?? does this mean HSE or a pin
-	TIM2->DIER |= TIM_DIER_TIE | TIM_DIER_UIE;
-	TIM2->PSC = 0xC350;
-	__NVIC_EnableIRQ(TIM2_IRQn);
-	TIM2->CR1 |= TIM_CR1_CEN;
-
-
-	GPIOD->MODER |= GPIO_MODER_MODE12_0 |
-					GPIO_MODER_MODE13_0 |
-					GPIO_MODER_MODE14_0 |
-					GPIO_MODER_MODE15_0; // set GPIO to "output" mode for LEDs
-
-
+	SYSTICK_CONFIG();
+	GPIO_CONFIG();
 	HSI_PLL_CLK_EN();
-
+	//TIM2_CONFIG();
 
 	RCC->CR |= RCC_CR_PLLON;
 	while(!(RCC->CR & RCC_CR_PLLRDY)){}
@@ -84,12 +52,9 @@ int main(void)
 	RCC->CFGR |= RCC_CFGR_PPRE1_2 | RCC_CFGR_SW_PLL;
 	while(!(RCC->CFGR & RCC_CFGR_SWS_PLL)){}
 
-
 	SystemCoreClockUpdate();
 
-
 	SysTick->LOAD |= SysTick_LOAD_RELOAD_Msk;
-
 
 	while(1){
 //		switch (BUTTON_MODE) {
@@ -114,7 +79,42 @@ int main(void)
 //		}
 		GPIOD->ODR |= LED_GREEN;
 	}
+}
 
+void SYSTICK_CONFIG(void){
+	PWR->CR |= PWR_CR_VOS;
+	FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS;
+	SysTick->LOAD &= SysTick_LOAD_RELOAD_Msk; // this is about 1 second
+	//NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
+	SysTick->VAL &= ~SysTick_VAL_CURRENT_Msk;
+	SysTick->CTRL |= 	SysTick_CTRL_CLKSOURCE_Msk 	|
+						SysTick_CTRL_TICKINT_Msk 	|
+						SysTick_CTRL_ENABLE_Msk;
+}
+void GPIO_CONFIG(void){
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIODEN;
+
+	GPIOD->MODER |= GPIO_MODER_MODE12_0 |
+					GPIO_MODER_MODE13_0 |
+					GPIO_MODER_MODE14_0 |
+					GPIO_MODER_MODE15_0; // set GPIO to "output" mode for LEDs
+}
+
+void TIM2_CONFIG(void){
+	// TIM2 clocked by pre-scaler output "CK_CNT"
+	// enable CEN before CK_CNT
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_PWREN;
+	//TIM2->ARR &= ~TIM_ARR_ARR;
+	//TIM2->ARR = 0x1F4;
+	//NVIC->ISER
+	//NVIC_SetPriority (TIM2_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
+	//TIM2->CNT &= ~TIM_CNT_CNT;
+	TIM2->EGR |= TIM_EGR_UG;
+	//TIM2->SMCR |= TIM_SMCR_ECE; // external clock enable?? does this mean HSE or a pin
+	//TIM2->DIER |= TIM_DIER_TIE | TIM_DIER_UIE;
+	//TIM2->PSC = 0xC350;
+	__NVIC_EnableIRQ(TIM2_IRQn);
+	TIM2->CR1 |= TIM_CR1_CEN;
 }
 
 void HSI_PLL_CLK_EN(void){
@@ -140,7 +140,6 @@ void HSI_PLL_CLK_EN(void){
 						RCC_PLLCFGR_PLLM_5	);
 
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSI;
-
 
 	RCC->CR |= RCC_CR_HSION;
 	while(!(RCC->CR & RCC_CR_HSIRDY)){}
@@ -170,10 +169,8 @@ void HSE_PLL_CLK_EN(void){
 
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
 
-
 	RCC->CR |= RCC_CR_HSEON;
 	while(!(RCC->CR & RCC_CR_HSERDY)){}
-
 }
 
 
@@ -182,9 +179,9 @@ void delay(uint32_t timeMS){
 }
 
 void SysTick_Handler(void){
-	//GPIOD->ODR ^= LED_BLUE;
+	GPIOD->ODR ^= LED_BLUE;
 }
 
 void TIM2_IRQHandler(void){
-	GPIOD->ODR |= LED_BLUE;
+	GPIOD->ODR |= LED_ORANGE;
 }
