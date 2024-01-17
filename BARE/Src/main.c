3641472d3c5bd0637b8e49d48fd687b26f9ff800
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-17 02:15:18 -0800

    clean and fixed pll values

--- BARE/Src/main.c
+++ BARE/Src/main.c
@@ -40,16 +40,18 @@ int main(void)
 {
 	SystemCoreClockUpdate();
 
+	PWR->CR |= PWR_CR_VOS;
+	FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS;
+
 	SYSTICK_CONFIG();
 	GPIO_CONFIG();
 	HSI_PLL_CLK_EN();
-	TIM2_CONFIG();
+	//TIM2_CONFIG();
 
 	RCC->CR |= RCC_CR_PLLON;
 	while(!(RCC->CR & RCC_CR_PLLRDY)){}
 
-
-	RCC->CFGR |= RCC_CFGR_PPRE1_2 | RCC_CFGR_SW_PLL;
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
 	while(!(RCC->CFGR & RCC_CFGR_SWS_PLL)){}
 
 	SystemCoreClockUpdate();
@@ -57,31 +59,12 @@ int main(void)
 	SysTick->LOAD |= SysTick_LOAD_RELOAD_Msk;
 
 	while(1){
-//		switch (BUTTON_MODE) {
-//		  case 0: //
-//				while(GPIOA->IDR & GPIO_IDR_IDR_0){
-//					GPIOD->ODR |= LED_GREEN;
-//				}
-//				GPIOD->ODR &= ~LED_GREEN;
-//		    break;
-//		  case 1:
-//				if(GPIOA->IDR & GPIO_IDR_IDR_0){
-//					GPIOD->ODR ^= LED_GREEN;
-//					delay(200); //de-bounce
-//				}
-//		    break;
-//		  default:
-//		}
-
-
-
 		GPIOD->ODR |= LED_GREEN;
 	}
 }
 
 void SYSTICK_CONFIG(void){
-	PWR->CR |= PWR_CR_VOS;
-	FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS;
+
 	SysTick->LOAD &= SysTick_LOAD_RELOAD_Msk; // this is about 1 second
 	//NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
 	SysTick->VAL &= ~SysTick_VAL_CURRENT_Msk;
@@ -89,6 +72,7 @@ void SYSTICK_CONFIG(void){
 						SysTick_CTRL_TICKINT_Msk 	|
 						SysTick_CTRL_ENABLE_Msk;
 }
+
 void GPIO_CONFIG(void){
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIODEN;
 
@@ -101,23 +85,31 @@ void GPIO_CONFIG(void){
 void TIM2_CONFIG(void){
 	// TIM2 clocked by pre-scaler output "CK_CNT"
 	// enable CEN before CK_CNT
+
+
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_PWREN;
 	//TIM2->ARR &= ~TIM_ARR_ARR;
-	//TIM2->ARR = 0x1F4;
+	TIM2->ARR = 0x31;
 	NVIC_SetPriority (TIM2_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL);
-	//TIM2->CNT &= ~TIM_CNT_CNT;
-	TIM2->EGR |= TIM_EGR_UG; // this sets TIM_SR_UIF
+
+
+	TIM2->EGR |= TIM_EGR_UG; // this sets TIM_SR_UIF & required for IRQ
 
 	//TIM2->SMCR |= TIM_SMCR_ECE; // external clock enable?? does this mean HSE or a pin
 	TIM2->DIER |= TIM_DIER_UIE; // TIM_DIER_TIE not sure what it does
-	//TIM2->PSC = 0x0;
-	__NVIC_EnableIRQ(TIM2_IRQn);
+	TIM2->PSC = 0xBB7F;
+
 	TIM2->CR1 |= TIM_CR1_CEN;
 
+	//while(!(TIM2->CR1 &= ~TIM_CR1_CEN));
+
+	//TIM2->CNT &= ~TIM_CNT_CNT;
 
 	if(TIM2->SR & TIM_SR_UIF){
 		GPIOD->ODR ^= LED_RED;
 	}
+
+	__NVIC_EnableIRQ(TIM2_IRQn);
 }
 
 void HSI_PLL_CLK_EN(void){
@@ -142,12 +134,15 @@ void HSI_PLL_CLK_EN(void){
 						RCC_PLLCFGR_PLLM_4 	|
 						RCC_PLLCFGR_PLLM_5	);
 
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSI;
 
 	RCC->CR |= RCC_CR_HSION;
 	while(!(RCC->CR & RCC_CR_HSIRDY)){}
 
 }
+
 void HSE_PLL_CLK_EN(void){
 	/* PLL 		M-4 	| N-192 	| P-4 	| Q-8
 	 * PLLI2S	M-5	| N-200		| R-2
@@ -170,12 +165,13 @@ void HSE_PLL_CLK_EN(void){
 						RCC_PLLCFGR_PLLM_4 	|
 						RCC_PLLCFGR_PLLM_5	);
 
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
 
 	RCC->CR |= RCC_CR_HSEON;
 	while(!(RCC->CR & RCC_CR_HSERDY)){}
 }
-
 
 void delay(uint32_t timeMS){
 	for(uint32_t i = 0; i < timeMS * 7059; i++){} //7059 is from experimentation
@@ -186,6 +182,25 @@ void SysTick_Handler(void){
 }
 
 void TIM2_IRQHandler(void){
+	//TIM2->SR &= ~TIM_SR_UIF;
+
 	GPIOD->ODR ^= LED_ORANGE;
-	__NVIC_DisableIRQ(TIM2_IRQn);
+	//__NVIC_DisableIRQ(TIM2_IRQn);
+	__disable_irq();
 }
+
+//		switch (BUTTON_MODE) {
+//		  case 0: //
+//				while(GPIOA->IDR & GPIO_IDR_IDR_0){
+//					GPIOD->ODR |= LED_GREEN;
+//				}
+//				GPIOD->ODR &= ~LED_GREEN;
+//		    break;
+//		  case 1:
+//				if(GPIOA->IDR & GPIO_IDR_IDR_0){
+//					GPIOD->ODR ^= LED_GREEN;
+//					delay(200); //de-bounce
+//				}
+//		    break;
+//		  default:
+//		}
