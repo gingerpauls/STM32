beadccd42aaf4edc57295cbb08b97b1c8940315f
GamePC\hutch <hutchinson9736@gmail.com>
2024-01-25 21:49:10 -0800

    added LCD code; changed measurement to 1us - need to test if correct

---
+++ BARE/Inc/lcd.h
@@ -0,0 +1,15 @@
+/* Configured for Sparkfun SerLCD v2.5
+ *
+ */
+#include "stm32f4xx.h"
+
+void Clear_Display(void);
+void Write_Character(char);
+void Write_String(char*);
+void Scroll_Right(void);
+void Scroll_Left(void);
+void Blink_Cursor(void);
+void Change_Baud_Rate(uint8_t);
+void Reset_Baud_Rate(void);
+void Backlight_OFF(void);
+void Backlight_ON(void);
